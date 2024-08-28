ff7098a03009050b36afc5ec9a012cba2dd5c7a1
Rishabh Gupta <rishabh9511@gmail.com>
2024-08-28 08:31:05 +0530

    Add grpc server/client code

---
+++ fuse_client/client.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	pb "google.golang.org/grpc/examples/route_guide/fuse_service" // Replace with the actual import path
+)
+
+var (
+	serverAddr = flag.String("addr", "localhost:50051", "The server address in the format of host:port")
+)
+
+func main() {
+	// Establish a connection to the gRPC server
+	var opts []grpc.DialOption
+	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	conn, err := grpc.NewClient(*serverAddr, opts...)
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	// Create a new client
+	client := pb.NewFuseClient(conn)
+
+	em := &pb.EmptyMessage{}
+	bl, err := client.ListBuckets(context.Background(), em)
+	if err != nil {
+		log.Fatalf("Failed to list buckets: %v", err)
+	}
+	fmt.Println(bl.AllBuckets)
+
+	// Call the RPC method
+	// req := &pb.MyRequest{Name: "World"}
+	// resp, err := client.MyMethod(context.Background(), req)
+	// if err != nil {
+	// 	log.Fatalf("could not greet: %v", err)
+	// }
+
+	// // Print the response
+	// fmt.Printf("Greeting: %s\n", resp.GetMessage())
+}
---
+++ fuse_server/example_server.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+
+	pb "google.golang.org/grpc/examples/route_guide/fuse_service"
+)
+
+type FuseServer struct {
+	pb.UnimplementedFuseServer
+}
+
+func (f *FuseServer) ListBuckets(ctx context.Context, em *pb.EmptyMessage) (*pb.Buckets, error) {
+
+	b1 := pb.Bucket{
+		Name: "tempBucket",
+	}
+
+	buckets := [...]*pb.Bucket{&b1}
+
+	return &pb.Buckets{
+		AllBuckets: buckets[:],
+	}, nil
+
+}
+
+func main() {
+
+	listener, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterFuseServer(s, &FuseServer{})
+
+	fmt.Println("Server is running on port 50051")
+	if err := s.Serve(listener); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+
+}
