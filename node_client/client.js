a289cc2807f4aa88d2d9dc1594460ea14bec7290
Rishabh Gupta <rishabh9511@gmail.com>
2024-08-29 21:03:19 +0530

    Fix the node client

--- node_client/client.js
+++ node_client/client.js
@@ -1,9 +1,12 @@
 const grpc = require('grpc');
 const protoLoader = require('@grpc/proto-loader');
+var messages = require('../fuse_service/example_pb')
 
 // Load the protobuf file
-const packageDefinition = protoLoader.loadSync('/home/zboon/fus3-app/fuse_service/example.proto');
+const packageDefinition = protoLoader.loadSync('/home/zboon/grpc-go/examples/route_guide/fuse_service/example.proto');
 const protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
+
+
 
 // Get the service definition
 const myService = protoDescriptor.fuse.Fuse
@@ -11,11 +14,12 @@ const myService = protoDescriptor.fuse.Fuse
 // Create a client instance
 const client = new myService('localhost:50051', grpc.credentials.createInsecure());
 
-// Call the RPC method
-// client.MyMethod({ name: 'World' }, (error, response) => {
-//   if (error) {
-//     console.error('Error:', error);
-//   } else {
-//     console.log('Response:', response.message);
-//   }
-// });
+var emptyMessage = new messages.EmptyMessage()
+
+client.ListBuckets(emptyMessage, (error, response) => {
+    if (error) {
+        console.error('Error:', error);
+    } else {
+        console.log('Response:', response);
+    }
+});
