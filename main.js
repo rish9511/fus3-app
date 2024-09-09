281b0f831bff15255e02ca2698e2a830ee43c5d7
Rishabh Gupta <rishabh9511@gmail.com>
2024-09-09 20:57:02 +0530

    Code cleanup in JS

--- main.js
+++ main.js
@@ -1,100 +1,135 @@
-// import "leaflet"
-// const { app, BrowserWindow } = require('electron')
-
-// const createWindow = () => {
-//   const win = new BrowserWindow({
-//     width: 800,
-//     height: 600
-//   })
-
-//   win.loadFile('index.html')
-// }
-// app.whenReady().then(() => {
-
-//   createWindow()
-// })
-
+// es6 style
+import L from "leaflet"
 // import "./node_modules/l.movemarker"
 
-import L from "leaflet"
-import "l.movemarker"
+// common js. why const ? // TODO: Common js syntax does not work
+// const {L} = require("./node_modules/leaflet")
 
-console.log("running main.js file")
-const map = L.map('map').setView([51.505, -0.09], 15);
 
-const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  maxZoom: 19,
-  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
-
-map.on('click', function(e) {
-  console.log(e.latlng.lat, e.latlng.lng)
+window.addEventListener('DOMContentLoaded', function(event) {
+  console.log("Dom Content loaded")
 });
 
 
-var aws_bucket_icon = L.icon({
-    iconUrl: 'aws-s3.svg',
-    // shadowUrl: 'leaf-shadow.png',
-    iconSize:     [38, 95], // size of the icon
-    shadowSize:   [50, 64], // size of the shadow
-    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-    shadowAnchor: [4, 62],  // the same for the shadow
-    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+window.addEventListener('load', async function(event) {
+  console.log("Loaded everything")
+  const map = L.map('map', {
+    center: L.latLng(0, 30),
+    zoom: 3,
+    minZoom: 3,
+    maxZoom: 3
+  });
+
+  const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(map);
+
+  // const response = await getBuckets()
+  // console.log(response)
+  // const allBuckets = response.getAllbucketsList()
+  // allBuckets.forEach(bucket => {
+  //   console.log(bucket.getName())
+  // })
+
+  getBuckets()
+    .then(buckets => {
+      buckets.forEach(bucket => {
+        console.log(bucket.getName())
+      })
+    })
+    .catch(err => {
+      console.log("Err: ", err)
+    })
 });
 
-var aws_bucket_icon_2 = L.icon({
-    iconUrl: 'aws-s3.svg',
-    // shadowUrl: 'leaf-shadow.png',
-    iconSize:     [38, 95], // size of the icon
-    shadowSize:   [50, 64], // size of the shadow
-    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-    shadowAnchor: [4, 62],  // the same for the shadow
-    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-});
 
+async function getBuckets() {
+    
+  const { EmptyMessage, Buckets } = require('./fuse_service/fuseservice_pb.js');
+  const { FuseClient, FusePromiseClient } = require('./fuse_service/fuseservice_grpc_web_pb.js');
 
-L.marker([51.5, -0.09], {icon: aws_bucket_icon}).addTo(map);
+  // TODO: can the same be done without using the promise client ? 
+  var client = new FusePromiseClient('http://localhost:8080');
 
-L.marker([51.50008749807709, -0.1257419586181641], {icon: aws_bucket_icon_2}).addTo(map);
+  var emptyMessage = new EmptyMessage();
 
-var polylineOptions = {
-  animate: true,
-  duration: 5000
-}
+  // client.listBuckets(emptyMessage, {})
+  //   .then(response => {
+  //     // const allBuckets = response.getAllbucketsList()
+  //     // allBuckets.forEach(bucket => {
+  //     //   console.log(bucket.getName())
+  //     // });
+  //     console.log("1")
+  //     return response
+  //     // console.log(response.getAllbucketsList())
+  //   })
+  //   .catch(err => {
+  //     console.log("Err: ", err)
+  //   })
 
-var markerOptions = {
-  animate: true,
-  duration: 5000
-}
-
-
-const instance = L.moveMarker(
-  [[51.5, -0.09], [51.50008749807709, -0.1257419586181641]],
-  polylineOptions,
-  markerOptions
-).addTo(map)
-
-
-console.log(instance.getMarker())
-
-
-
-const { EmptyMessage } = require('./fuse_service/fuseservice_pb.js');
-const { FuseClient } = require('./fuse_service/fuseservice_grpc_web_pb.js');
-
-var client = new FuseClient('http://localhost:8080');
-
-var emptyMessage = new EmptyMessage();
-
-client.listBuckets(emptyMessage, {}, (err, response) => {
-  if (err) {
-    console.error('Error:', err);
-  } else {
-    console.log('Response:', response);
+  try {
+    const response = await client.listBuckets(emptyMessage, {})
+    return response.getAllbucketsList()
+  } catch(err) {
+    console.log("Err: ", err)
   }
-});
+
+  // client.listBuckets(emptyMessage, {}, (err, response) => {
+  //   if (err) {
+  //     console.error('Error:', err);
+  //   } else {
+  //     console.re
+  //     return response
+  //   }
+  // });
+}
 
 
+
+// var aws_bucket_icon = L.icon({
+//     iconUrl: 'aws-s3.svg',
+//     // shadowUrl: 'leaf-shadow.png',
+//     iconSize:     [38, 95], // size of the icon
+//     shadowSize:   [50, 64], // size of the shadow
+//     iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+//     shadowAnchor: [4, 62],  // the same for the shadow
+//     popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+// });
+
+// var aws_bucket_icon_2 = L.icon({
+//     iconUrl: 'aws-s3.svg',
+//     // shadowUrl: 'leaf-shadow.png',
+//     iconSize:     [38, 95], // size of the icon
+//     shadowSize:   [50, 64], // size of the shadow
+//     iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+//     shadowAnchor: [4, 62],  // the same for the shadow
+//     popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+// });
+
+
+// L.marker([51.5, -0.09], {icon: aws_bucket_icon}).addTo(map);
+
+// L.marker([51.50008749807709, -0.1257419586181641], {icon: aws_bucket_icon_2}).addTo(map);
+
+// var polylineOptions = {
+//   animate: true,
+//   duration: 5000
+// }
+
+// var markerOptions = {
+//   animate: true,
+//   duration: 5000
+// }
+
+
+// const instance = L.moveMarker(
+//   [[51.5, -0.09], [51.50008749807709, -0.1257419586181641]],
+//   polylineOptions,
+//   markerOptions
+// ).addTo(map)
+
+
+// console.log(instance.getMarker())
 
 
 // instance.addMoreLine([-8.822512, 115.186803], {
