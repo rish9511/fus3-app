aeb3c15ba5e42d3b43b44c36f70c93d3f93cd635
Rishabh Gupta <rishabh9511@gmail.com>
2024-11-01 22:19:52 +0530

    Make server load and return static buckets when serving ListBuckets endpoint

--- server/server.go
+++ server/server.go
@@ -14,19 +14,61 @@ type FuseServer struct {
 	pb.UnimplementedFuseServer
 }
 
+type StaticBuckets map[string]map[string]string
+
 func (f *FuseServer) ListBuckets(ctx context.Context, em *pb.EmptyMessage) (*pb.Buckets, error) {
 
-	fmt.Println("Received request")
-	b1 := pb.Bucket{
-		Name: "tempBucket",
+	sBuckets := loadStaticBuckets()
+
+	/*
+		{
+			'Bucket1': {
+				'Name': 'xyz'
+				'Path': 'abc'
+			}
+		}
+	*/
+
+	var buckets []*pb.Bucket
+	for _, v := range sBuckets {
+		bucket := pb.Bucket{
+			Name: v["Name"],
+			Path: v["Path"],
+		}
+
+		buckets = append(buckets, &bucket)
+
 	}
 
-	buckets := [...]*pb.Bucket{&b1}
+	fmt.Println("Received request")
+	// b1 := pb.Bucket{
+	// 	Name: "tempBucket",
+	// }
+
+	// buckets := [...]*pb.Bucket{&b1}
 
 	return &pb.Buckets{
 		AllBuckets: buckets[:],
 	}, nil
 
+}
+
+func loadStaticBuckets() StaticBuckets {
+	buckets := make(StaticBuckets)
+	buckets["Bucket1"] = make(map[string]string)
+	buckets["Bucket2"] = make(map[string]string)
+
+	buckets["Bucket1"]["Name"] = "B1"
+	buckets["Bucket1"]["Path"] = "/buckets/b1"
+	// buckets["Bucket1"]["Latitude"] = "12.345"
+	// buckets["Bucket1"]["Longitude"] = "0"
+
+	buckets["Bucket2"]["Name"] = "B2"
+	buckets["Bucket2"]["Path"] = "/buckets/b2"
+	// buckets["Bucket2"]["Latitude"] = "40.345"
+	// buckets["Bucket2"]["Longitude"] = "0"
+
+	return buckets
 }
 
 func main() {
